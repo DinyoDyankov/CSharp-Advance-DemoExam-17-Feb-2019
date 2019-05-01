c39e6e58a15f0dd0ea8f29e848599f95e6b6a2ac
Calabria88 <Calabria88@students.softuni.bg>
2019-05-01 11:46:21 +0300

    Add project files.

---
+++ ExcelFunctions/Program.cs
@@ -0,0 +1,110 @@
+﻿namespace ExcelFunctions
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int sizeOfMatrix = int.Parse(Console.ReadLine());
+
+            string[][] matrix = new string[sizeOfMatrix][];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] matrixData = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                matrix[row] = matrixData;
+            }
+
+            string[] command = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string header = command[1];
+
+            if (command[0] == "hide")
+            {
+                int indexOfHeader = GetIndexOfHeader(matrix, header);
+
+                var matrixAfterDeletion = MatrixHideColumn(matrix, indexOfHeader);
+
+                for (int row = 0; row < matrixAfterDeletion.Length; row++)
+                {
+                    Console.WriteLine(string.Join(" | ",matrixAfterDeletion[row]));
+                }
+            }
+            else if (command[0] == "sort")
+            {
+                int indexOfHeader = GetIndexOfHeader(matrix, header);
+
+                string[] headerRow = matrix[0];
+
+                Console.WriteLine(string.Join(" | ", headerRow));
+
+                matrix = matrix.OrderBy(x => x[indexOfHeader]).ToArray();
+
+                foreach (var row in matrix)
+                {
+                    if (row != headerRow)
+                    {
+                        Console.WriteLine(string.Join(" | ", row));
+                    }
+                }
+
+            }
+            else if (command[0] == "filter")
+            {
+                string commandToReturn = command[2];
+
+                string[] headerRow = matrix[0];
+
+                Console.WriteLine(string.Join(" | ", headerRow));
+
+                int indexOfHeader = GetIndexOfHeader(matrix, header);
+
+                for (int row = 0; row < matrix.Length; row++)
+                {
+                    for (int col = 0; col < matrix[row].Length; col++)
+                    {
+                        if (col == indexOfHeader && matrix[row][col] == commandToReturn)
+                        {
+                            Console.WriteLine(string.Join(" | ", matrix[row]));
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private static string[][] MatrixHideColumn(string[][] matrix, int indexOfHeader)
+        {
+            string[][] matrixWithHiddenColumn = new string[matrix.Length][];
+
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                matrixWithHiddenColumn[row] = new string[matrix[row].Length - 1];
+                int colIndex = 0;
+
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    if (col == indexOfHeader)
+                    {
+                        continue;
+                    }
+
+                    matrixWithHiddenColumn[row][colIndex] = matrix[row][col];
+                    colIndex++;
+                }
+            }
+
+            return matrixWithHiddenColumn;
+        }
+
+        private static int GetIndexOfHeader(string[][] matrix, string header)
+        {
+            int headerIndex = Array.IndexOf(matrix[0], header);
+
+            return headerIndex;
+        }
+    }
+}
---
+++ Socks/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Socks
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] leftSocksInput = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] rightSocksInput = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Stack<int> leftSocks = new Stack<int>();
+            Queue<int> rightSocks = new Queue<int>();
+
+            List<int> allPairsOfSocks = new List<int>();
+
+            for (int i = 0; i < leftSocksInput.Length; i++)
+            {
+                leftSocks.Push(leftSocksInput[i]);
+            }
+
+            for (int i = 0; i < rightSocksInput.Length; i++)
+            {
+                rightSocks.Enqueue(rightSocksInput[i]);
+            }
+
+            while (leftSocks.Count != 0 && rightSocks.Count != 0)
+            {
+                if (leftSocks.Peek() > rightSocks.Peek())
+                {
+                    int pairSum = leftSocks.Pop() + rightSocks.Dequeue();
+
+                    allPairsOfSocks.Add(pairSum);
+                }
+                else if (leftSocks.Peek() == rightSocks.Peek())
+                {
+                    rightSocks.Dequeue();
+
+                    int currentLeftSockValue = leftSocks.Pop() + 1;
+
+                    leftSocks.Push(currentLeftSockValue);
+                }
+                else if (leftSocks.Peek() < rightSocks.Peek())
+                {
+                    leftSocks.Pop();
+                }
+            }
+
+            int biggestPair = -1;
+
+            foreach (var pair in allPairsOfSocks)
+            {
+                if (biggestPair < pair)
+                {
+                    biggestPair = pair;
+                }
+            }
+
+            Console.WriteLine(biggestPair);
+            Console.WriteLine(string.Join(' ', allPairsOfSocks));
+        }
+    }
+}
